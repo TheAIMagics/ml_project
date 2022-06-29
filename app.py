1082e33dd04691406ab4992ddf80711da56554af
satyazmx <thakursatya6@gmail.com>
2022-06-29 17:44:44 +0530

    Add logging and exception

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
+import imp
+import sys
 from flask import Flask
+from housing.logger import logging
+from housing.exception import HousingException
 
 app = Flask(__name__)
 
 @app.route('/', methods=['POST','GET'])
 def index():
+    try:
+        raise Exception("We are testing custom Exception")
+    except Exception as e:
+        housing = HousingException(e,sys)
+        logging.info(housing.error_message)
+        logging.info("We are testing logging module")
     return "<h1>Hello Pipeline</h1>"
 
 if __name__=='__main__':
---
+++ test.py
@@ -0,0 +1,22 @@
+from typing import List
+
+REQUIREMENT_FILE_NAME="requirements.txt"
+
+def get_requirements_list()->List[str]:
+    """
+    Description: This function is going to return list of requirement 
+    mention in requirements.txt file
+
+    return This function is going to return a list which contain name 
+    of libraries mentioned in requirements.txt file
+    """
+    with open(REQUIREMENT_FILE_NAME) as requirement_file:
+        requirement_list = [lib_name.replace("\n","") for lib_name in requirement_file.readlines()]
+
+        if "-e ." in requirement_list:
+            requirement_list.remove("-e .")
+        print(requirement_list)
+        return requirement_list
+        
+if __name__=='__main__':
+    get_requirements_list()
