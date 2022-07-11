f459d23d8aa770c61f4e19533c3b402b21caebfb
satyazmx <thakursatya6@gmail.com>
2022-07-11 16:16:05 +0530

    add data transformation to pipeline

--- demo.py
+++ demo.py
@@ -1,11 +1,20 @@
 import logging
-from xml.etree.ElementTree import PI
 from housing.pipeline.pipeline import Pipeline
+from housing.component.data_transformation import DataTransformation
+import os
 
 def main():
     try:
         pipeline = Pipeline()
         pipeline.run_pipeline()
+        #data_validation_config = Configuartion().get_data_transformation_config()
+        #print(data_validation_config)
+        #schema_file_path = r"D:\GIT\project\ml_project\config\schema.yaml"
+        #file_path = r"D:\GIT\project\ml_project\housing\artifact\data_ingestion\2022-07-07-23-00-13\ingested_data\train\housing.csv"
+        #df = DataTransformation.get_data_transformer_object()
+        #print(df.columns)
+        #print(df.dtypes)
+        
     except Exception as e:
         logging.error(f"{e}")
         print(e)
